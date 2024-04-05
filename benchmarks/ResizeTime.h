545db1f06a96c990247363021aaa693d5223ec2e
Roberto Vicario <rvicariodev@gmail.com>
2024-04-05 17:23:04 +0200

    Added 'ResizeTime' benchmark

--- benchmarks/ResizeTime.h
+++ benchmarks/ResizeTime.h
@@ -6,20 +6,37 @@
 
 #include <chrono>
 #include <iostream>
+#include <random>
 
 using namespace std;
 using namespace std::chrono;
 
 template <typename Algorithm>
-int computeResizeTime(string algorithm, uint32_t anchor_set, uint32_t working_set) {
+int computeResizeTime(string algorithm, uint32_t anchor_set,
+                uint32_t working_set) {
     Algorithm engine(anchor_set, working_set);
 
+    /**
+     * Selecting random bucket index.
+     */
+    random_device rd;
+    mt19937 rng(rd());
+    uint32_t index = rng() % working_set;
+
+    /**
+     * Starting measuring.
+     */
     auto start{clock()};
 
-    // CODE
+    vector<uint32_t> bucket_status(working_set, 1);
+    auto removed_node = engine.removeBucket(index);
+    bucket_status[removed_node] = 0;
 
     auto end{clock()};
 
+    /**
+     * Printing results.
+     */
     auto time{static_cast<double>(end - start) / CLOCKS_PER_SEC};
     cout << "# [LOG] ----- @" << algorithm << "\t>_ resize_time    = " << time << " seconds" << endl;
 
