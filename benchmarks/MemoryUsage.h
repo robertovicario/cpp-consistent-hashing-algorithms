b085411a328105dae928a7ecf31e3a1348e2e673
Roberto Vicario <rvicariodev@gmail.com>
2024-04-05 12:49:14 +0200

    updates

--- benchmarks/MemoryUsage.h
+++ benchmarks/MemoryUsage.h
@@ -9,8 +9,8 @@ using namespace std;
 
 template <typename Algorithm>
 int computeMemoryUsage(string algorithm, uint32_t anchor_set, uint32_t working_set) {
-    Algorithm obj(anchor_set, working_set);
-    size_t size = sizeof(obj);
+    Algorithm engine(anchor_set, working_set);
+    size_t size = sizeof(engine);
     cout << "# [LOG] ----- @" << algorithm << "\t>_ memory_usage   = " << size << " bytes" << endl;
 
     return 0;
